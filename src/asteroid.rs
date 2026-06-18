1af1518feffc8936cd917001fbe096bdc6bbc237
Justin W <justincomputerwilliams@gmail.com>
2026-06-18 16:48:16 -0400

    Asteroid physics + wrapping

--- src/asteroid.rs
+++ src/asteroid.rs
@@ -5,8 +5,9 @@ use crate::vec_util;
 
 const MAX_ASTERIOD_EDGES: u8 = 9;
 const MIN_ASTEROID_EDGES: u8 = 5;
-const ASTEROID_SPEED: f32 = 100.0;
+const INITIAL_ASTEROID_SPEED: f32 = 50.0;
 
+#[derive(Debug, Copy, Clone)]
 pub enum AsteroidSize {
     Small,
     Medium,
@@ -14,7 +15,7 @@ pub enum AsteroidSize {
 }
 
 impl AsteroidSize {
-    fn radius(&self) -> f32 {
+    pub fn radius(&self) -> f32 {
         match self {
             AsteroidSize::Small => 7.5,
             AsteroidSize::Medium => 15.0,
@@ -24,29 +25,32 @@ impl AsteroidSize {
 }
 
 pub struct Asteroid {
+    id: i32,
     size: AsteroidSize,
     position: Vec2,
     rotation: f32,
     verticies: Vec<Vec2>,
     spin: f32,
-    direction: Vec2,
+    velocity: Vec2,
 }
 
 impl Asteroid {
-    pub fn new(size: AsteroidSize, position: Vec2) -> Asteroid {
+    pub fn new(id: i32, size: AsteroidSize, position: Vec2) -> Asteroid {
         let edges = rand::gen_range(MIN_ASTEROID_EDGES, MAX_ASTERIOD_EDGES + 1);
         let rotation = rand::gen_range(0.0, TAU);
         let verticies = Self::gen_verticies(&size, edges);
         let spin = rand::gen_range(-PI, PI);
         let direction = Self::gen_direction();
+        let velocity = direction * INITIAL_ASTEROID_SPEED;
 
         Asteroid {
+            id,
             size,
             position,
             rotation,
             verticies,
             spin,
-            direction,
+            velocity,
         }
     }
 
@@ -73,6 +77,27 @@ impl Asteroid {
         (self.position, self.size.radius())
     }
 
+    pub fn velocity(&self) -> Vec2 {
+        self.velocity
+    }
+
+    pub fn set_velocity(&mut self, velocity: Vec2) {
+        self.velocity = velocity;
+    }
+
+    pub fn size(&self) -> AsteroidSize {
+        self.size
+    }
+
+    pub fn position(&self) -> Vec2 {
+        self.position
+    }
+
+    pub fn set_position(&mut self, position: Vec2) {
+        self.position = position;
+        self.wrap_position();
+    }
+
     pub fn gen_position(avoid_pos: Vec2, min_clearance: f32, other_asteroids: &[Asteroid]) -> Vec2 {
         loop {
             let candidate = vec2(
@@ -82,7 +107,7 @@ impl Asteroid {
             let mut overlaps_other = false;
             for other in other_asteroids {
                 if candidate.distance(other.position)
-                    < (other.size.radius() + AsteroidSize::Large.radius())
+                    < 2.0 * (other.size.radius() + AsteroidSize::Large.radius())
                 {
                     overlaps_other = true;
                     break;
@@ -95,32 +120,55 @@ impl Asteroid {
     }
 
     pub fn draw(&self) {
+        let w = screen_width();
+        let h = screen_height();
+        let r = self.size.radius();
+        let x = self.position.x;
+        let y = self.position.y;
+
+        let mut x_centers: Vec<f32> = vec![x];
+        if x + r > w {
+            x_centers.push(x - w);
+        } else if x - r < 0.0 {
+            x_centers.push(x + w);
+        }
+
+        let mut y_centers = vec![y];
+        if y + r > h {
+            y_centers.push(y - h);
+        } else if y - r < 0.0 {
+            y_centers.push(y + h);
+        }
+
+        for x in &x_centers {
+            for y in &y_centers {
+                self.draw_at(vec2(*x, *y));
+            }
+        }
+    }
+
+    fn draw_at(&self, center: Vec2) {
         let len = self.verticies.len();
+
         for i in 0..len {
-            let start = self.position + vec_util::rotate(self.rotation, self.verticies[i]);
-            let end =
-                self.position + vec_util::rotate(self.rotation, self.verticies[(i + 1) % len]);
+            let start = center + vec_util::rotate(self.rotation, self.verticies[i]);
+            let end = center + vec_util::rotate(self.rotation, self.verticies[(i + 1) % len]);
 
             draw_line(start.x, start.y, end.x, end.y, 1.5, WHITE);
         }
     }
 
     pub fn update(&mut self, dt: f32) {
-        let velocity = self.direction * ASTEROID_SPEED;
-        self.position += velocity * dt;
-
-        if self.position.x > screen_width() {
-            self.position.x = 0.0;
-        } else if self.position.x < 0.0 {
-            self.position.x = screen_width();
-        }
-        if self.position.y > screen_height() {
-            self.position.y = 0.0;
-        } else if self.position.y < 0.0 {
-            self.position.y = screen_height();
-        }
+        self.position += self.velocity * dt;
+        self.wrap_position();
 
         self.rotation += self.spin * dt;
         self.rotation = self.rotation.rem_euclid(TAU);
+    }
+
+    fn wrap_position(&mut self) {
+        self.position.x = self.position.x.rem_euclid(screen_width());
+
+        self.position.y = self.position.y.rem_euclid(screen_height());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -23,14 +23,7 @@ async fn main() {
     let ship_pos = vec2(screen_width() / 2.0, screen_height() / 2.0);
     let mut ship = ship::Ship::new(ship_pos);
 
-    //                    ~ship_radius + buffer
-    let asteroid_clearance = 20.0 + 5.0;
-
-    let mut asteroids: Vec<asteroid::Asteroid> = Vec::with_capacity(ASTEROID_COUNT);
-    for _ in 0..ASTEROID_COUNT {
-        let pos = asteroid::Asteroid::gen_position(ship_pos, asteroid_clearance, &asteroids);
-        asteroids.push(asteroid::Asteroid::new(asteroid::AsteroidSize::Large, pos));
-    }
+    let mut asteroids = create_asteroids(ship_pos);
 
     let mut hit = false;
 
@@ -39,19 +32,71 @@ async fn main() {
 
         let dt = get_frame_time();
 
+        if is_key_pressed(KeyCode::Enter) {
+            paused = !paused;
+        }
+
         if !paused {
+            // Position updates
             ship.update(dt);
             for a in &mut asteroids {
                 a.update(dt);
-                a.draw();
             }
+
+            // Collision detection
+
+            // Asteroid <-> spacehsip collision
             hit = asteroids.iter().any(|a| {
                 let (ship_pos, ship_rad) = ship.bounds();
                 let (a_pos, a_rad) = a.bounds();
-                circles_overlap(ship_pos, ship_rad, a_pos, a_rad)
+                circles_overlap_wrapped(ship_pos, ship_rad, a_pos, a_rad)
             });
-        }
 
+            // Asteroid <-> asteroid collision
+
+            // Detect
+            let mut collisions: Vec<(usize, usize)> = Vec::new();
+            for i in 0..asteroids.len() {
+                let (a_i_pos, a_i_rad) = asteroids[i].bounds();
+                for j in i + 1..asteroids.len() {
+                    let (a_j_pos, a_j_rad) = asteroids[j].bounds();
+
+                    if circles_overlap_wrapped(a_i_pos, a_i_rad, a_j_pos, a_j_rad) {
+                        collisions.push((i, j));
+                    }
+                }
+            }
+
+            // Resolve
+            for (i, j) in collisions {
+                let [a_i, a_j] = asteroids
+                    .get_disjoint_mut([i, j])
+                    .expect("i and j are distinct");
+
+                let (a_i_pos, a_i_rad) = a_i.bounds();
+                let (a_j_pos, a_j_rad) = a_j.bounds();
+
+                let delta = wrapped_delta(a_i_pos, a_j_pos);
+                let dist = delta.length();
+                let n = if dist < f32::EPSILON {
+                    vec2(1.0, 0.0)
+                } else {
+                    delta / dist
+                };
+
+                let rel = a_i.velocity() - a_j.velocity();
+                let along = rel.dot(n);
+                if along > 0.0 {
+                    a_i.set_velocity(a_i.velocity() - (along * n));
+                    a_j.set_velocity(a_j.velocity() + (along * n));
+                }
+
+                let overlap = (a_i_rad + a_j_rad) - dist;
+                let separation = n * (overlap * 0.5);
+                a_i.set_position(a_i_pos - separation);
+                a_j.set_position(a_j_pos + separation);
+            }
+        }
         if hit {
             paused = true;
         }
@@ -65,12 +110,54 @@ async fn main() {
             draw_centered_outlined("HIT", 80.0, WHITE, BLACK);
         }
 
+        if paused && hit && is_key_down(KeyCode::Space) {
+            ship = ship::Ship::new(ship_pos);
+            asteroids = create_asteroids(ship_pos);
+            hit = false;
+            paused = false;
+        }
+
         next_frame().await
     }
 }
 
-fn circles_overlap(c1: Vec2, r1: f32, c2: Vec2, r2: f32) -> bool {
-    c1.distance_squared(c2) < (r1 + r2).powi(2)
+fn create_asteroids(ship_pos: Vec2) -> Vec<asteroid::Asteroid> {
+    let mut asteroids = Vec::with_capacity(ASTEROID_COUNT);
+
+    //                    ~ship_radius + buffer
+    let asteroid_clearance = 20.0 + 150.0;
+
+    for i in 0..ASTEROID_COUNT {
+        let pos = asteroid::Asteroid::gen_position(ship_pos, asteroid_clearance, &asteroids);
+        asteroids.push(asteroid::Asteroid::new(
+            i as i32,
+            asteroid::AsteroidSize::Large,
+            pos,
+        ));
+    }
+    asteroids
+}
+
+fn circles_overlap_wrapped(c1: Vec2, r1: f32, c2: Vec2, r2: f32) -> bool {
+    wrapped_delta(c1, c2).length_squared() < (r1 + r2).powi(2)
+}
+
+fn wrapped_delta(p1: Vec2, p2: Vec2) -> Vec2 {
+    let mut dx = p2.x - p1.x;
+    if dx > (screen_width() / 2.0) {
+        dx -= screen_width();
+    } else if dx < -(screen_width() / 2.0) {
+        dx += screen_width();
+    }
+
+    let mut dy = p2.y - p1.y;
+    if dy > (screen_height() / 2.0) {
+        dy -= screen_height();
+    } else if dy < -(screen_height() / 2.0) {
+        dy += screen_height();
+    }
+
+    vec2(dx, dy)
 }
 
 fn draw_centered_outlined(text: &str, font_size: f32, fill: Color, outline: Color) {
